cb69218f14ada1ad53d24f074d2171ec1f299a41
grafMonteVictor <victorpindrus@gmail.com>
2025-05-25 22:42:30 +0300

    added logger in exception

--- spring-postgre-app/src/main/java/com/example/spring_postgre_app/controller/ErrorController.java
+++ spring-postgre-app/src/main/java/com/example/spring_postgre_app/controller/ErrorController.java
@@ -1,17 +1,19 @@
 package com.example.spring_postgre_app.controller;
 
 import com.example.spring_postgre_app.model.ErrorInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@ControllerAdvice
-/*
-* spring, перехвати исключения из любого контроллера*/
+@ControllerAdvice //spring, перехвати ответ из контроллера
+@Slf4j
+
 public class ErrorController {
-    @ExceptionHandler(Exception.class)
-    @ResponseBody
+    @ExceptionHandler(Exception.class) //spring, перехвати ответ-исключение
+    @ResponseBody //spring, ответ в виде json
     public ErrorInfo processException(Exception e) {
+        log.error(e.getMessage());
         return new ErrorInfo(e.getMessage());
     }
 
